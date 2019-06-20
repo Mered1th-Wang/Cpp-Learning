565fe8e8943ebd5d6abdc7c98dff2a662e426048
Mered1th-Wang <504102143@qq.com>
2019-06-20 14:32:26 +0800

    update spell_v3

--- project/src3/TestTcpServer.cc
+++ project/src3/TestTcpServer.cc
@@ -1,17 +1,5 @@
-#include "Threadpool.h"
-#include "TcpServer.h"
-#include "EditDistance.h"
-#include "MyTask.h"
-#include "MyDict.h"
-#include "MyConf.h"
 #include "SpellCorrectServer.h"
 
-#include <unistd.h>
-#include <stdio.h>
-#include <string.h>
-#include <iostream>
-
-using namespace std;
 using namespace wd;
 
 MyDict* MyDict::pInstance_ = new MyDict();  //饱汉模式
